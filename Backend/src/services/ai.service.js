00c396f860572471fdc155448521b9741af565c6
eshakar <102459513+eshakar@users.noreply.github.com>
2026-04-02 21:07:29 +0530

    Fixed low memory

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -263,7 +263,17 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 
 async function generatePdfFromHtml(htmlContent) {
     const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ],
+        headless: "new"
     });
     const page = await browser.newPage();
     await page.setContent(htmlContent, { waitUntil: "networkidle0" })
