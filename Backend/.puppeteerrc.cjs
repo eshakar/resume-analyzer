9fcfff6a1d90d658ec02a31bf84a39b0d364add9
eshakar <102459513+eshakar@users.noreply.github.com>
2026-04-13 14:30:40 +0530

    not needed

--- Backend/.puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const { join } = require('path');
-
-/**
- * @type {import('puppeteer').Configuration}
- */
-module.exports = {
-  // Changes the cache location for Puppeteer so Render does not delete it between builds.
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
